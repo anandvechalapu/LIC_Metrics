69496b220ce00fed2e3addc07019eee544e3916e
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-09 14:37:20 +0530

    initial commit

---
+++ LIC_Metrics/src/main/java/com/lic/metrics/model/.java
@@ -0,0 +1,21 @@
+package com.lic.metrics.model;
+
+import java.util.Map;
+
+import com.lic.metrics.exceptions.ApplicationException;
+import com.lic.metrics.exceptions.RequiredFieldException;
+import com.lic.metrics.model.dto.CommonResponseDto;
+
+public interface ProcessPolicyMemberByServiceIdSupportController5Repository {
+	
+	/**
+	 * API endpoint to process bulk policy members' merger/conversion fund-related details using a service ID, so that member 
+	 * transaction entries/debits can be made efficiently
+	 *
+	 * @param serviceId The serviceId path parameter should be a valid Long value
+	 * @return CommonResponseDto<Map<String,String>> containing the status of the request and an appropriate message
+	 * @throws RequiredFieldException If the {serviceId} is invalid, throw a RequiredFieldException with an appropriate error message
+	 * @throws ApplicationException If an ApplicationException is thrown, catch it and set the CommonResponseDto status field to ERROR, with an appropriate error message in the response message field
+	 */
+	public CommonResponseDto<Map<String, String>> processPolicyMemberByServiceId(Long serviceId) throws RequiredFieldException, ApplicationException;
+}
